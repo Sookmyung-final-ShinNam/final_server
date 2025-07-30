f12f8747e0bb9b61c4fa2b9872cf337e687c8685
yoon <sin020511@gmail.com>
2025-07-30 23:55:17 +0900

    🩹chore: user 엔티티에 관리자용 등급 추가

--- src/main/java/com/example/demo/domain/user/entity/User.java
+++ src/main/java/com/example/demo/domain/user/entity/User.java
@@ -40,6 +40,11 @@ public class User extends BaseEntity {
     @Column(name = "current_points", nullable = false)
     private int currentPoints = 500;
 
+    // 사용자 등급 (기본값 BASIC)
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserGrade grade = UserGrade.BASIC;
+
     // 사용자 상태 (기본값 ACTIVE)
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
@@ -68,6 +73,11 @@ public class User extends BaseEntity {
     // 약관 1 : 스토리와 캐릭터는 운영자만 삭제 가능합니다. 삭제를 원할 시 별도 신청/문의가 필요합니다.
     //         추후 공유 기능 개발 예정이고 다른 사용자들이 캐릭터와 유대감을 쌓을 수 있으므로 중대한 이유가 아닌 이상 삭제 불가합니다.
     // 약관 2 : 사용자 대화 기록과 즐겨찾기 여부는 탈퇴시 자동으로 삭제됩니다.
+
+    public enum UserGrade {
+        BASIC,    // 기본 사용자
+        ADMIN     // 관리자
+    }
 
     public enum UserStatus {
         ACTIVE,   // 로그인
