51e4df850b8ad01439aef4aa047006386066aa21
yoon <sin020511@gmail.com>
2025-08-12 16:46:04 +0900

    🩹chore: 이름으로 배경/테마 목록 조회하는 메소드 추가

--- src/main/java/com/example/demo/domain/story/repository/BackgroundRepository.java
+++ src/main/java/com/example/demo/domain/story/repository/BackgroundRepository.java
@@ -3,5 +3,15 @@ package com.example.demo.domain.story.repository;
 import com.example.demo.domain.story.entity.Background;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BackgroundRepository extends JpaRepository<Background, Long> {
+
+    /**
+     * 배경 이름으로 배경을 조회합니다.
+     *
+     * @param name 배경 이름
+     * @return 배경 엔티티
+     */
+    Optional<Background> findByName(String name);
 }
--- src/main/java/com/example/demo/domain/story/repository/ThemeRepository.java
+++ src/main/java/com/example/demo/domain/story/repository/ThemeRepository.java
@@ -3,5 +3,15 @@ package com.example.demo.domain.story.repository;
 import com.example.demo.domain.story.entity.Theme;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ThemeRepository extends JpaRepository<Theme, Long> {
+
+    /**
+     * 테마 이름으로 테마를 조회합니다.
+     *
+     * @param name 테마 이름
+     * @return 테마 엔티티
+     */
+    Optional<Theme> findByName(String name);
 }
