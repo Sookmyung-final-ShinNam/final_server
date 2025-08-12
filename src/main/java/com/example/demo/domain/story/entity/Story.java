f4d857e473fe2d2225a8e03d4c81b29f780dacad
yoon <sin020511@gmail.com>
2025-08-12 20:07:33 +0900

    ♻️refactor: null 방지 - 기본값 세팅

--- src/main/java/com/example/demo/domain/story/entity/Story.java
+++ src/main/java/com/example/demo/domain/story/entity/Story.java
@@ -43,23 +43,28 @@ public class Story extends BaseEntity {
     private User user;
 
     // 스토리 삭제 시 스토리 테마도 삭제
+    @Builder.Default
     @OneToMany(mappedBy = "story", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<StoryTheme> storyThemes = new HashSet<>();
 
     // 스토리 삭제 시 스토리 배경도 삭제
+    @Builder.Default
     @OneToMany(mappedBy = "story", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<StoryBackground> storyBackgrounds = new HashSet<>();
 
     // 스토리 삭제 시 스토리 페이지도 삭제
+    @Builder.Default
     @OneToMany(mappedBy = "story", cascade = CascadeType.ALL, orphanRemoval = true)
     @OrderBy("pageNumber ASC")
     private Set<StoryPage> storyPages = new HashSet<>();
 
     // 스토리 삭제 시 대화 세션도 삭제
+    @Builder.Default
     @OneToMany(mappedBy = "story", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<ConversationSession> storySessions = new HashSet<>();
 
     // 스토리 삭제 시 스토리 즐겨찾기도 삭제
+    @Builder.Default
     @OneToMany(mappedBy = "story", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<UserStoryFavorite> userStoryFavorites = new HashSet<>();
 
