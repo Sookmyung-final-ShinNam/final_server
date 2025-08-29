d30992b01c834b8f269fa6806421f8878a5ebc7a
yoon <sin020511@gmail.com>
2025-08-29 22:33:05 +0900

    🩹chore: JVM 기본 시간대 설정

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.TimeZone;
+
 @EnableAsync
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		// JVM 기본 시간대 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
